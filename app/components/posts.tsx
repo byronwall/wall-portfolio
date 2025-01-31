8a9753c051684cfa4861127583fb7d7d5ce18469
Byron Wall <byron@byroni.us>
2025-01-31 14:38:30 -0500

    Fix date and styles

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,11 +21,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="flex flex-col space-y-1 mb-4 hover:bg-neutral-100 dark:hover:bg-neutral-800 p-4 rounded-md"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:gap-4">
+              <p className="shrink-0 text-neutral-600 dark:text-neutral-400  tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
